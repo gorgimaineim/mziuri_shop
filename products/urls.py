0868ad7ceecc4451a77218c673709d02a31aa575
g0r81 <dagiojanadze@gmail.com>
2024-12-07 19:15:02 +0400

    addded products

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('product/', views.product_detail, name='product_detail')
 ]
